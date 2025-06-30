a13f1115a6459cb1646e1d1bd6942db5906d1fdc
Toby Cm <toby@vsus.app>
2025-06-30 14:50:12 -0700

    proper size

--- web/src/components/SongsList.tsx
+++ web/src/components/SongsList.tsx
@@ -42,7 +42,7 @@ export default function SongsList() {
     <ScrollArea h="60vh" w={isMobile ? "100%" : "42vw"} bdrs="xl" style={{ boxShadow: theme.shadows.lg, backdropFilter: "blur(24px)" }}>
       <Stack gap="md" p="md">
         {songs.data?.map((song) => {
-          song.albumArtOptions = { width: 64, height: 64 }; // Set album art options for the song
+          song.albumArtOptions = { height: 80 }; // Set album art options for the song
           return <SongCard song={song} key={song.id} selected={nowPlayingId === song.id} playing={nowPlayingId === song.id ? playing : false} />;
         })}
       </Stack>
